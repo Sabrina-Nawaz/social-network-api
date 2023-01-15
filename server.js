f6a6102c4d71ae6fad2cfe9516e4bb6878a84e1a
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2023-01-15 18:13:29 -0500

    Created config file to connect to mongoose and added comments to server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./config/connection");
 
+// Establish express and the PORT
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-mongoose.connect(process)
+// Establishes successful connection to the PORT
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
+
+mongoose.connect(process);
