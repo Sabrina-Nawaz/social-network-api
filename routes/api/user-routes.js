e62ab0ce6d36148d0128243290e18dfcaede655e
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2023-01-17 21:11:45 -0500

    Finished adding routes to user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -14,6 +14,9 @@ const {
 router.route("/").get(getUsers).post(createUser);
 
 // Route to GET, PUT and DELETE one user /api/users/:id
-router.route("/:id").get(get);
+router.route("/:id").get(getOneUser).put(updateUser).delete(deleteUser);
+
+// Route to add and delete a friend
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
